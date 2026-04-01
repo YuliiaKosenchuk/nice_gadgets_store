37eb0c816bae62b37a1d85afc85a134c3b6d6a31
Yuliia Kosenchuk <yuliia.kosenchuk@gmail.com>
2026-04-01 20:59:29 +0200

    add config

--- vite.config.ts
+++ vite.config.ts
@@ -5,9 +5,17 @@ import path from 'path';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/nice_gadgets_store/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
+    },
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        silenceDeprecations: ['legacy-js-api', 'global-builtin', 'import'],
+      },
     },
   },
 });
